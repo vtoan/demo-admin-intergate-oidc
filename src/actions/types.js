0ce147a54d4d526762a52779b828776da6fa863e
DangToan <dtoan2805@gmail.com>
2021-04-21 08:10:17 -0700

    Add Actions Redux

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+export const STORE_USER = "STORE_USER";
+export const USER_SIGNED_OUT = "USER_SIGNED_OUT";
+export const USER_EXPIRED = "USER_EXPIRED";
+export const STORE_USER_ERROR = "STORE_USER_ERROR";
+export const LOADING_USER = "LOADING_USER";
+export const SET_TYPE_PRODUCT = "SET_TYPE_PRODUCT";
+export const SET_CATEGORY = "SET_CATEGORY";
